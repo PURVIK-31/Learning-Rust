60450ab22ed7e5104190636f26ca1513aa9074f0
PURVIK-31 <mepurviksharma@gmail.com>
2025-03-22 15:07:09 +0530

    Bool data type in rust"

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 fn main() {
-    let ans = sum(1,2);
-    println!(" The sum of digits is :  {}", ans)
+    let num :bool = check_even(12);
+    println!("{}", num);
 }
 
-fn sum(a: u32, b: u32) -> u32{
-    return a + b;
-}
+// fn sum(a: u32, b: u32) -> u32{
+//     return a + b;
+// }
 
+fn check_even(a:u32)->bool{
+return a%2==0;
+}
