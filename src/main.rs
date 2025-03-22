6e5c3bab5860101165e63e9bde9608e98667fadd
PURVIK-31 <mepurviksharma@gmail.com>
2025-03-22 15:32:29 +0530

    if else in rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
-    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    let arr = [1, 2, 3, 4];
+    if arr.len() == 5 {
     println!("{}", arr.len());
+    }
+    else{
+    println!("Array length is not 5");
+    }
 }
