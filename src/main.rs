3ebd8387d08496febb27ece35cce667202a4f525
PURVIK-31 <mepurviksharma@gmail.com>
2025-03-22 23:49:46 +0530

    Ownership in rust

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,8 @@
-
-//time for learning loops in rust : 
+//ownerships in rust!! 
 fn main() {
-    // let my_name = "Rust";
-    // let mut counter = 0; 
-   //simplest type of loop, controlled using break statement! 
-    // loop {
-    //     counter += 1;
-    //     if counter == 5 {
-    //         break;
-    //     }
-    //     else{
-    //         println!("Hello from {}!", my_name);
-    //     }
-    // }
-
-    //for loop in rust
-    // for i in 1..5{
-    //     println!("{}",i);
-    // }
-    //while loop in rust : 
-    let mut counter = 0;
-    while counter < 5 {
-        println!("Hello from number:  {}", counter);
-        counter += 1;
-    }
+let string1 = String::from("phehwiehfiwehf");
+let string2 = string1;
+// print!("{}", string1); // this will give an error because string1 has been moved to string2
+print!("{}", string2);
+//this is allowed as the current ownership of string1 has been moved to string2
 }
