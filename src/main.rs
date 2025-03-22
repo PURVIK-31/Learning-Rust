3dabfe85daaed90c7aeb840e37ee0b6b711430ed
PURVIK-31 <mepurviksharma@gmail.com>
2025-03-22 15:41:25 +0530

    for loop in rust

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 
 //time for learning loops in rust : 
 fn main() {
-    let my_name = "Rust";
-    let mut counter = 0; 
-    loop {
-        counter += 1;
-        if counter == 5 {
-            break;
-        }
-        else{
-            println!("Hello from {}!", my_name);
-        }
+    // let my_name = "Rust";
+    // let mut counter = 0; 
+   //simplest type of loop, controlled using break statement! 
+    // loop {
+    //     counter += 1;
+    //     if counter == 5 {
+    //         break;
+    //     }
+    //     else{
+    //         println!("Hello from {}!", my_name);
+    //     }
+    // }
+
+    //for loop in rust
+    for i in 1..5{
+        println!("{}",i);
     }
 }
