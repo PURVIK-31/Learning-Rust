dab8ac1ca09a182bd62e91172ca6ce546271c888
PURVIK-31 <mepurviksharma@gmail.com>
2025-03-22 15:00:05 +0530

    Unsigned integers in sum function !!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     let ans = sum(1,2);
     println!(" The sum of digits is :  {}", ans)
-} 
+}
 
-fn sum(a: i32, b: i32) -> i32{
+fn sum(a: u32, b: u32) -> u32{
     return a + b;
 }
 
