415222df0ca0f8af0c95a6c6ef6a490fab8763c7
PURVIK-31 <mepurviksharma@gmail.com>
2025-03-24 18:39:51 +0530

    Options and Some -- handling none values in rust

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,25 @@
-enum Shape {
-    Circle(u32),
-    Square(u32, u32),
-    Rectangle(u32, u32),
+//options and result enums in rust
+//used for: 
+//1. null values 
+//2. error handling
+
+//1. Option enum
+//Option enum is used to handle null values in rust
+//rust doesnt allow null values in its code, hence we use options for this
+fn main(){ 
+let index = find_first_a(String::from("hello"));
+match index {
+    Some(value)=> println!("The index of first a is: {}", value),
+    None => println!("There is no a in the string")
+}
 }
 
-fn main() {
-    let shape1 = Shape::Circle(2); // Circle with radius 2
-    let shape2 = Shape::Square(2, 2); // Square with sides 2
-    let shape3 = Shape::Rectangle(2, 2); // Rectangle with width 2 and height 2
 
-    match shape1 {
-        Shape::Circle(radius) => println!("Circle with radius {}", radius),
-        Shape::Square(width, height) => println!("Square with width {} and height {}", width, height),
-        Shape::Rectangle(width, height) => println!("Rectangle with width {} and height {}", width, height),
+fn find_first_a(s:String)->Option<u32>{
+    for (i, c) in s.chars().enumerate(){
+        if c == 'o'{
+            return Some(i as u32);
+        }
     }
-    let areaof: u32 = area(shape1);
-    println!("Area of shape1 is {}", areaof);
-    let areaof: u32 = area(shape2);
-    println!("Area of shape2 is {}", areaof);       
-    let areaof: u32 = area(shape3);
-    println!("Area of shape3 is {}", areaof);
-}
-
-fn area(s:Shape)->u32{
-    match s{
-        Shape::Circle(r)=>3*r*r,
-        Shape::Square(w,h)=>w*h,
-        Shape::Rectangle(w,h)=>w*h,
-    }
+    None
 }
