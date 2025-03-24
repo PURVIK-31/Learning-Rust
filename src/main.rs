f94e50c5fbed99adee8f2f8e2f6fb2d7fdf70649
PURVIK-31 <mepurviksharma@gmail.com>
2025-03-25 00:31:47 +0530

    Mutable iter

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
-//iterators in rust
-
-// defining a iterator
-// iterator is a type of rust that implements the iterator trait
-// iterator trait is defined in the standard library
-// iterator trait has a method called next that returns an option
-// option is an enum that has two variants Some and None
-// Some contains the next value and None indicates that the iterator is finished    
-
-//iterators are lazy, they do nothing until you call the next method
-//iterators are also composable, you can chain them together to perform complex operations
-
-fn main(){
-    let val = vec![1,2,3,4,5];
-let vak = val.iter();       
+//mutable iterator 
+ fn main(){
+    let mut val = vec![1,2,3,4,5];
+let vak = val.iter_mut();
     for i in vak{
-    println!("{}",i);
-}
-
-}
+    println!("{}",*i+1);
+}}
