9bd9bdaaaa6c36ff035faebf1cf070dada9984d2
PURVIK-31 <mepurviksharma@gmail.com>
2025-03-25 01:07:12 +0530

    Intoiter  --> take ownership and revoke the orignal collection

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-//mutable iterator 
- fn main(){
-    let  val = vec![1,2,3,4,5];
-    let mut iter = val.iter();
-    while let Some(val) = iter.next(){
-        println!("val: {}", val);
-    }
+//into intertor is a special type of iterator which has few benefits with conditions as per belowl:
+
+// this trait converts the collection to an iterator and takes the ownership of the collection
+
+//useful when:
+//one no longer needs the original collection
+//avoiding references and take performance benefits 
+//avoiding lifetime issues
+
+
+fn main() {
+    let v = vec![1, 2, 3];
+    let sum: i32 = v.into_iter().sum();
+    println!("sum: {}", sum);
 }
