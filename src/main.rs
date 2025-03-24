1b8a3867faa0b8564c6eaaccda3929fe0f94add7
PURVIK-31 <mepurviksharma@gmail.com>
2025-03-24 23:06:44 +0530

    vectors in rust!!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-//use chrono:
-use chrono::Utc;
+//lets start with some advanced rust concepts :
 
+//collections framework in rust!
+
+
+//vectors --> dynamic arrays : 
 
 fn main(){
-    let nowtime = Utc::now();
-    println!("The current time is  {}", nowtime);
-} 
+    let mut vector = Vec::new();
+    vector.push(1);
+    vector.push(2);
+    vector.push(3);
+    vector.pop();
+    print!("{}",vector.capacity());
+}
