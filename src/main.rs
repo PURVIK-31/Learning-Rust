9c2353643ee7857f4cdf83f7b8f294deef7e418f
PURVIK-31 <mepurviksharma@gmail.com>
2025-03-24 23:10:03 +0530

    another way to print

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,12 @@ fn main(){
     vector.push(1);
     vector.push(2);
     vector.push(3);
-    vector.pop();
-    print!("{}",vector.capacity());
+    vector.pop();  // This removes the last element (3)
+    println!("{}",vector.capacity());
+    println!("{}",vector[0]);
+    println!("{}",vector[1]);
+    // The index 2 no longer exists after pop()
+    // You can add this back if you need it:
+    // vector.push(3);
+    // println!("{}",vector[2]);
 }
