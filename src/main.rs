c6965434be14b197cf8eeab4ebe1fc6ef7cdf457
PURVIK-31 <mepurviksharma@gmail.com>
2025-03-24 19:58:49 +0530

    importing and printing date and time

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
-//error handling in rust is done using the Result enum
-//lets have a look below:
-// Result enum is defined as:
-// enum Result<T, E>{
-//     Ok(T),
-//     Err(E),
-// }
-//hence, there are two return statements which we can use to return the value from the function
-// Ok(T) -> returns the value of type T 
-// Err(E) -> returns the value of type E
-//lets have a look at the example below:
-use std::fs;
+//use chrono:
+use chrono::Utc;
+
 
 fn main(){
-let read_file = fs::read_to_string("a.txt");    
-match read_file {
-    Ok(file) => {
-        println!("File content is: {}", file);
-    },
-    Err(error) => {
-        println!("Error reading file: {}", error);
-    }
-}
-}
+    let nowtime = Utc::now();
+    println!("The current time is  {}", nowtime);
+} 
