e7561535d966dc9ac2143b2d537ab3165579356c
PURVIK-31 <mepurviksharma@gmail.com>
2025-03-25 00:35:23 +0530

    Next and while loop logic in iterator

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 //mutable iterator 
  fn main(){
-    let mut val = vec![1,2,3,4,5];
-let vak = val.iter_mut();
-    for i in vak{
-    println!("{}",*i+1);
-}}
+    let  val = vec![1,2,3,4,5];
+    let mut iter = val.iter();
+    while let Some(val) = iter.next(){
+        println!("val: {}", val);
+    }
+}
