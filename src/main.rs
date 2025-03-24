13b85c214bb820c06a620071198f2f26227152a3
PURVIK-31 <mepurviksharma@gmail.com>
2025-03-24 23:48:07 +0530

    Converting a vector into a hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::collections::HashMap;
 
-//Hashmaps in Rust
-fn main(){
-    let mut mymap =  HashMap::new();
-    mymap.insert(String::from("myname1"), 22);
-    mymap.insert(String::from("myname2"), 23);
-    let getval = mymap.get("noen");
-    match getval {
-        Some(val)=> println!("{:?}", val),
-
-        None => println!("No value found"),
-        
-        
+//assinment problem solution:
+fn convert_to_hashmap(vec:Vec<(String,i32)>)->HashMap<String,i32>{
+    let mut hm=  HashMap::new();
+    for (key,value) in vec{
+        hm.insert(key, value);
     }
+  return hm;
 }
+
+fn main(){
+    let vector = vec![(String::from("myname1"),21),(String::from("myname2"),22),(String::from("myname3"),23)];
+    let hm = convert_to_hashmap(vector);
+    println!("{:?}",hm);
+    }
