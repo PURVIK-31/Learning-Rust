b73cca95c55ef1c19b1bda76dcff8371eabac8f4
PURVIK-31 <mepurviksharma@gmail.com>
2025-03-26 03:30:37 +0530

    filter function in rust

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
-//into intertor is a special type of iterator which has few benefits with conditions as per belowl:
-
-// this trait converts the collection to an iterator and takes the ownership of the collection
-
-//useful when:
-//one no longer needs the original collection
-//avoiding references and take performance benefits 
-//avoiding lifetime issues
-
-
-fn main() {
-    let v = vec![1, 2, 3];
-    let sum: i32 = v.into_iter().sum();
-    println!("sum: {}", sum);
+fn main(){
+    let vec1 = vec![1, 2, 3];
+    let iter  = vec1.iter();
+    let filtered_iter = iter.filter(|x| *x % 2 == 0);
+    print!("{:?}", filtered_iter.collect::<Vec<&i32>>());  
 }
